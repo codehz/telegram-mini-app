8f46fa6801f3adef926af712ff5f0ae907f394a9
codehz <git@hertz.moe>
2024-08-18 22:26:24 +0800

    add initial height

--- components/FieldSet.tsx
+++ components/FieldSet.tsx
@@ -1,5 +1,5 @@
 import { tw } from "bun-tailwindcss" with { type: "macro" };
-import type { ReactNode } from "react";
+import type { ComponentProps, ReactNode } from "react";
 import { AnimatedHeight } from "./AnimatedHeight";
 
 export function FieldSet({
@@ -7,19 +7,24 @@ export function FieldSet({
   children,
   footer,
   dynamic,
+  initialHeight,
 }: {
   title?: string;
   children: ReactNode;
   footer?: ReactNode;
   dynamic?: boolean;
+  initialHeight?: number | undefined;
 }) {
   return (
-    <div>
-      {!!title && <legend className={tw("text-hint mx-4 mb-1 text-xs font-extralight")}>{title}</legend>}
+    <div className={tw("pointer-events-children")}>
+      {!!title && (
+        <legend className={tw("text-hint pointer-events-none mx-4 mb-1 text-xs font-extralight")}>{title}</legend>
+      )}
       {dynamic ? (
-        <div className={tw("bg-secondary-bg rounded-2xl shadow-md contain-paint")}>
+        <div className={tw("bg-secondary-bg auto-visibility rounded-2xl shadow-md contain-paint")}>
           <AnimatedHeight
             className={tw("grid", "divide-divide divide-y", "*:first:rounded-t-2xl *:last:rounded-b-2xl")}
+            initialHeight={initialHeight}
           >
             {children}
           </AnimatedHeight>
@@ -27,7 +32,7 @@ export function FieldSet({
       ) : (
         <div
           className={tw(
-            "bg-secondary-bg grid rounded-2xl shadow-md contain-paint",
+            "bg-secondary-bg auto-visibility grid rounded-2xl shadow-md contain-paint",
             "divide-divide divide-y",
             "*:first:rounded-t-2xl *:last:rounded-b-2xl",
           )}
@@ -38,4 +43,27 @@ export function FieldSet({
       {footer}
     </div>
   );
+}
+
+export namespace FieldSet {
+  export function Button(props: ComponentProps<"button">) {
+    return (
+      <button
+        {...props}
+        className={tw(
+          "focus-visible:bg-button focus-visible:text-button-text text-accent-text flex w-full cursor-pointer appearance-none items-center justify-center gap-2 py-3 px-4 !outline-none transition-colors",
+        )}
+      />
+    );
+  }
+  export function DangerButton(props: ComponentProps<"button">) {
+    return (
+      <button
+        {...props}
+        className={tw(
+          "text-destructive-text focus-visible:bg-destructive-text focus-visible:text-bg flex w-full cursor-pointer appearance-none items-center justify-center gap-2 py-3 px-4 !outline-none transition-colors",
+        )}
+      />
+    );
+  }
 }
