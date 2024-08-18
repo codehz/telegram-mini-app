bb73eaff981e9eea7c0fe9f649e38d83791cd93e
codehz <git@hertz.moe>
2024-08-18 22:25:31 +0800

    add initial height

--- components/AnimatedHeight.tsx
+++ components/AnimatedHeight.tsx
@@ -4,10 +4,11 @@ import { useLayoutEffect, useRef, useState, type ComponentProps } from "react";
 export function AnimatedHeight({
   children,
   ref: parentRef,
+  initialHeight = 0,
   ...props
-}: ComponentProps<"div">) {
+}: ComponentProps<"div"> & { initialHeight?: number | undefined }) {
   const ref = useRef<HTMLDivElement>(null);
-  const [height, setHeight] = useState(0);
+  const [height, setHeight] = useState(initialHeight);
   useLayoutEffect(() => {
     const observer = new ResizeObserver(([entry]) => {
       setHeight(entry.contentRect.height);
@@ -17,9 +18,7 @@ export function AnimatedHeight({
   }, []);
   return (
     <div
-      className={tw(
-        "relative box-content overflow-hidden transition-[height] contain-strict",
-      )}
+      className={tw("relative box-content overflow-hidden transition-[height] contain-strict")}
       style={{ height }}
       ref={parentRef}
     >
