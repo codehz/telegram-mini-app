8c4efa28f820db837cfb0fab76e28752dbfc400e
codehz <git@hertz.moe>
2024-08-21 00:43:54 +0800

    fix toast dismiss

--- hooks/useToastDismiss.ts
+++ hooks/useToastDismiss.ts
@@ -5,7 +5,7 @@ export function useToastDismiss() {
   const ref = useRef<(string | number)[]>([]);
   useEffect(() => () => {
     ref.current.map((key) => toast.dismiss(key));
-  });
+  }, []);
   return useCallback((id: string | number) => {
     ref.current.push(id);
   }, []);
