ffffd8427c28c54dcd01c856712477dffa03cb53
codehz <git@hertz.moe>
2024-08-29 15:21:51 +0800

    add portal

--- components/Shadow.tsx
+++ components/Shadow.tsx
@@ -1,14 +1,17 @@
 import { useLayoutEffect, useRef, useState, type ComponentPropsWithoutRef, type ElementRef } from "react";
+import { createPortal } from "react-dom";
 
 export function Shadow({
   ["shadow:className"]: shadowClassName,
   ["shadow:height"]: shadowHeight = false,
   ["shadow:width"]: shadowWidth = false,
+  portal,
   ...props
 }: {
   ["shadow:className"]?: string;
   ["shadow:height"]?: boolean;
   ["shadow:width"]?: boolean;
+  portal?: Element | DocumentFragment;
 } & ComponentPropsWithoutRef<"div">) {
   const [size, setSize] = useState<{ width: number; height: number }>({
     width: 0,
@@ -29,7 +32,7 @@ export function Shadow({
         }}
         className={shadowClassName}
       />
-      <div {...props} ref={ref} />
+      {portal ? createPortal(<div {...props} ref={ref} />, portal) : <div {...props} ref={ref} />}
     </>
   );
 }
